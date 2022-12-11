5a17c50298f2481b5ecaff42b5620adb3b1b1604
m3ro1986 <m3ro_1986@outlook.com>
2022-12-10 23:39:25 -0600

    add product funtion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'assets/images/featured1.png',
+      category: 'hoodies',
+      quantity: 10,
+      quantitySelected: 0
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'assets/images/featured2.png',
+      category: 'shirts',
+      quantity: 15,
+      quantitySelected: 0
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'assets/images/featured3.png',
+      category: 'shirts',
+      quantity: 20,
+      quantitySelected: 0
+    }
+  ]
+
 window.addEventListener ( 'scroll', () => { document.querySelector( 'header' ).classList.toggle( 'scroll-bar', window.scrollY > 0 ) })
 
 function changeTheme () {
@@ -26,3 +56,24 @@ document.getElementById('hib-1').addEventListener( 'mouseout' , () => {
     document.getElementById('hib-1').classList.replace('hib-2', 'hib-1')
     document.getElementById('hib-2').classList.replace('hib-1', 'hib-2')
 })
+
+function sumCart( array ) {
+    let sum = array.map( ele => ele.quantitySelected ).reduce((a, b) => a + b)
+    let mul = array.map( ele => ele.quantitySelected * ele.price ).reduce((a, b) => a + b)
+
+    return [sum, mul]
+}
+
+
+function addProduct( id ){
+    // let item = items.find( ele => { ele.id === id})
+    
+    if ( items[ id - 1 ].quantitySelected < items[ id - 1 ].quantity) {
+        items[ id - 1 ].quantitySelected++
+    }else{
+        alert("¡No puede agregar mas productos porque ya no hay en stock!")
+    }
+
+    document.getElementById( 'counter' ).innerHTML = sumCart( items )[0]
+
+}
